019230a9be99ff25c1ec8b80558e2c51dce580c2
Жовохир <133970072+Jovohir17@users.noreply.github.com>
2024-03-17 18:28:34 +0500

    first commit

---
+++ src/Pages/Master.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Main from '../Components/Main'
+import Header from '../componentss/Header'
+import Mainn from '../componentss/Mainn'
+import Main__Footer from "../Components/Main__Footer";
+import Footer from "../Components/Footer";
+
+function Master() {
+  return (
+    <>
+      <Header/>
+      <Mainn/>
+      <Main/>
+      <Main__Footer/>
+      <Footer/>
+    </>
+  )
+}
+
+export default Master
